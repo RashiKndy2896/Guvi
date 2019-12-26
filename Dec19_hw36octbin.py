6754804c7ed240fecd35f25e9c3f7b652afa65c7
RashiKndy2896 <58968996+RashiKndy2896@users.noreply.github.com>
2019-12-26 14:04:14 +0530

    Update Dec19_hw36octbin.py

--- Dec19_hw36octbin.py
+++ Dec19_hw36octbin.py
@@ -1,8 +1,21 @@
-print("Enter 'x' for exit.");
-octal = input("Enter any number in Octal Format: ");
-if octal == 'x':
-    exit();
-else:
-    dec = str(int(octal, 8));
-    decm = int(dec);
-    print(octal,"in Binary =",bin(decm));
+import math
+def octal_to_binary(octal):
+decimal = 0
+i = 0
+binary = 0
+while(octal != 0):
+decimal += (octal%10) * math.pow(8,i)
+i = i + 1
+octal = int (octal / 10)
+
+i = 1
+
+while (decimal != 0):
+binary = binary + (decimal % 2) * i;
+decimal = int(decimal / 2);
+i *= 10;
+
+return binary
+
+octal = int(input(“Enter an octal number : “))
+print(“Binary Equivalent : “,int(octal_to_binary(octal)))
